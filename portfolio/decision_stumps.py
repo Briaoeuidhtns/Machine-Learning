a68fe5bbda50cdf499fc00311c32124a37f87983
Brian Brunner <brian.brunner.bb@gmail.com>
2020-12-08 14:54:55 -0700

    Update documentation and visualizations

--- portfolio/decision_stumps.py
+++ portfolio/decision_stumps.py
@@ -6,27 +6,26 @@ from operator import itemgetter
 from typing import Sequence
 
 import numpy as np
+
 from . import visualize
 
 
 @dataclass
 class DecisionStumps(visualize.DataView):
     """Least Squares regression adapted from book section 9.2.1"""
-    x: Sequence[Real]
-    y: Sequence[Real]
     theta: Real = field(init=False)
     x_j: Sequence[Real] = field(init=False)
     error: Real = field(init=False)
 
-    def __post_init__(self):
-        self.x = np.array(self.x)
-        self.y = np.array(self.y)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # was a bit confused on the optimized pseudocode in the book,
         # but to the best of my knowledge this is what the book wanted
         # for the O(dm^2) method
-        self.error, self.x_j, self.theta = min(((np.sum(np.sign(x_j-theta) != self.y), x_j, theta)
-                                                for x_j in self.x.T
-                                                for theta in x_j), key=itemgetter(0))
+        self.error, self.x_j, self.theta = min(
+            ((np.sum(np.sign(x_j - theta) != self.y), x_j, theta)
+             for x_j in self.x.T for theta in x_j),
+            key=itemgetter(0))
 
     def plot(self, axes):
         axes.plot(self.x_j, self.y, 'o')
--- portfolio/visualize.py
+++ portfolio/visualize.py
@@ -1,37 +1,71 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
+from numbers import Real
 from tempfile import NamedTemporaryFile
-from typing import Type
+from typing import Sequence, Type
 
 import matplotlib.pyplot as plt
 import mpld3
+import numpy as np
 from matplotlib.axes import Axes
+from base64 import standard_b64encode
+import io
 
 
 class Plot:
     def __init__(self, x, y, label='data'):
-        self.x = x
-        self.y = y
+        self.x = np.array(x)
+        self.y = np.array(y)
         self.fig = plt.figure()
         self.__axes = self.fig.add_subplot()
-        # self.__axes.plot(self.x, self.y, 'o', label=label)
 
-    def add_view(self, L: Type[DataView]):
-        L(self.x, self.y).plot(self.__axes)
+    def add_view(self, L: Type[DataView], **kwargs):
+        L(self.x, self.y, **kwargs).plot(self.__axes)
         self.__axes.legend()
 
-    def save(self):
-        with NamedTemporaryFile(delete=False, suffix='.png', dir='../data') as f:
+    def save(self, dir='../data'):
+        with NamedTemporaryFile(delete=False, suffix='.png',
+                                dir='../data') as f:
             self.fig.savefig(f)
             return f.name
 
-    def embed(self):
-        return mpld3.fig_to_html(self.fig)
+    def show(self):
+        plt.show(self.fig)
+
+    def embed(maybe_self, static=True):
+        """Exports as a string suitable for html embedding
+
+        Can also be run as a static function to export a fig.
+        """
+        if isinstance(maybe_self, Plot):
+            self = maybe_self
+            fig = self.fig
+        else:
+            self = None
+            fig = maybe_self
+
+        if static:
+            with io.BytesIO() as img:
+                fig.savefig(img, format='png')
+                with img.getbuffer() as bimg:
+                    return (
+                        b'<img src="data:image/png;charset=utf-8;base64, %b" />'
+                        % standard_b64encode(bimg)).decode()
+        else:
+            return mpld3.fig_to_html(fig)
 
 
+@dataclass
 class DataView:
-    def __init__(x, y):
-        pass
+    x: Sequence[Real]
+    y: Sequence[Real]
+    label: str = 'data'
 
     def plot(self, axes: Axes):
         pass
+
+
+class Points(DataView):
+    def plot(self, axes: Axes):
+        axes.plot(self.x, self.y, 'o', label=self.label)
